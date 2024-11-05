4f05f0dc92ccbced93f796dca3a0aba22b1d5fae
mark <markhughesster@gmail.com>
2024-11-05 12:52:03 +0000

    installed JwtBearer & Web packages for Az AD (entra) Auth

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.Identity.Web;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to enable authentication with Azure AD
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseRouting();
+
 app.MapControllers();
+
 app.Run();
+
+
 
