13ca109fb6600736bc10d299931d03c14f0a2123
mark <markhughesster@gmail.com>
2024-11-05 11:23:16 +0000

    Backend Senind to FE successfully

---
+++ Controllers/Values.Controller.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = new string[] { "value1", "value2", "value3" };
+            return Ok(values);
+        }
+    }
+}
--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e4ffaaf8c841acdd5d37b4d82e4dc937516891")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
